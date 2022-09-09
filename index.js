3784dddc11d2216659134b6380d8070fb4661aba
+ <+>
2022-09-09 23:02:35 +0300

    assignment done

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+function titleCased(){
+  const capTutorials = tutorials.map(t => {
+    const words = t.split(" ").map(w => {
+      return w.charAt(0).toUpperCase() + w.slice(1);
+    }).join(" ");
+    console.log(words);
+    return words; 
+  })
+  return capTutorials;
+}
